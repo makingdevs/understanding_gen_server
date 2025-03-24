8217fdbe8429c598c1f740ae20f25a66a9ed1e8c
José Juan Reyes Zuñiga <juan@makingdevs.com>
2025-03-24 14:00:53 -0600

    Should Be Alive!!!

--- priv/processes.ex
+++ priv/processes.ex
@@ -11,3 +11,11 @@ pid3 = FibonacciServer.start()
 send pid3, {caller, 20}
 
 IO.inspect [pid1, pid2, pid3]
+
+Process.alive?(pid1)
+Process.alive?(pid2)
+Process.alive?(pid3)
+
+send pid1, {caller, 10}
+send pid2, {caller, 15}
+send pid3, {caller, 20}
