380cfdaebc9139ea424b00176837b7d196e60869
José Juan Reyes Zuñiga <juan@makingdevs.com>
2025-03-25 00:51:10 -0600

    Link and Monitor Process

--- priv/processes.ex
+++ priv/processes.ex
@@ -13,3 +13,8 @@ GenericServer.cast(pid, {:compute, 30})
 GenericServer.call(pid, {:status})
 
 send pid, {:any, :noop}
+
+Process.flag(:trap_exit, true)
+Process.exit pid, :kill
+Process.monitor(pid)
+Process.exit(pid, :kill)
